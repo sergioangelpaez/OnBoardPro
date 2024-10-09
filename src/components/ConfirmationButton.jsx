b5ef49c1f38cd81edfc832f87094a77509bfe61d
Sergio Angel <glasspalms.software@gmail.com>
2024-10-09 14:56:29 -0500

    Added components
    Added ConfirmationButton and GreyContainer components.

---
+++ src/components/ConfirmationButton.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/ConfirmationButton.module.scss';
+
+const ConfirmationButton = ({}) => {
+    return(
+        <button className={styles.button}>Confirm</button>
+    );
+}
+
+export default ConfirmationButton;
---
+++ src/components/GreyContainer.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/GreyContainer.module.scss';
+
+const GreyContainer = ({}) => {
+    return(
+        <div className={styles.GreyContainer} />
+    );
+}
+
+export default GreyContainer;
